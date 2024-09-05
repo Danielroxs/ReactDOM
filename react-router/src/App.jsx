630b9633a4f39c404c52df13ac73762146cee0f6
Danielroxs <danroxsll@gmail.com>
2024-09-04 20:03:26 -0600

    ReactDOM

---
+++ react-router/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Enrutador from "../src/section/Home.jsx";
+
+function App() {
+  return (
+    <>
+      <Enrutador />
+    </>
+  );
+}
+
+export default App;
---
+++ react-router/src/section/Home.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
+
+function Contact() {
+  return <h2>Contact Page</h2>;
+}
+
+function Enrutador() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        contact
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Enrutador;
